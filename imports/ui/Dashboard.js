88930a113d8ce48ac3e0441907c4c9bd3c5e5350
Tony Gagliardi <tonygags@internode.on.net>
2017-09-01 18:49:18 +0930

    Init commit

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PrivateHeader from './PrivateHeader';
+
+export default () => {
+  return (
+        <div>
+          <PrivateHeader title="Dashboard"/>
+          <div className="page-content">
+          Dashboard Page Content
+          </div>
+        </div>
+      );
+ };
