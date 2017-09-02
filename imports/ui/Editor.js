9330bfede901fb1e17c87a450fe5be520d06838f
Tony Gagliardi <tonygags@internode.on.net>
2017-09-02 21:32:14 +0930

    Fix editor error

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -7,45 +7,39 @@ import { browserHistory } from 'react-router';
 import { Notes } from '../api/notes';
 
 export class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
+  handleBodyChange(e) {
+    const body = e.target.value;
+    this.setState({ body });
+    this.props.call('notes.update', this.props.note._id, { body });
+  }
+  handleTitleChange(e) {
+    const title = e.target.value;
+    this.setState({ title });
+    this.props.call('notes.update', this.props.note._id, { title });
+  }
+  handleRemoval(){
+    this.props.call('notes.remove', this.props.note._id);
+    this.props.browserHistory.push('/dashboard');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const currentNoteId = this.props.note ? this.props.note._id : undefined;
+    const prevNoteId = prevProps.note ? prevProps.note._id : undefined;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: '',
-            body: ''
-        };
+    if (currentNoteId && currentNoteId !== prevNoteId) {
+      this.setState({
+        title: this.props.note.title,
+        body: this.props.note.body
+      });
     }
-
-    handleBodyChange(e) {
-        const body = e.target.value;
-        this.setState({ body })
-        this.props.call('notes.update', this.props.note._id, { body });
-    }
-
-    handleTitleChange(e) {
-        const title = e.target.value;
-        this.setState({ title })
-        this.props.call('notes.update', this.props.note._id, { title });
-    }
-
-    handleRemoval() {
-        this.props.call('notes.remove', this.props.note._id);
-        this.props.browserHistory.push('/dashboard');
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        const currentNoteId = this.props.note ? this.props.note._id : undefined ;
-        const prevNoteId = prevProps.note ? prevProps.note._id : undefined ;
-
-        if (currentNoteId && currentNoteId !== prevNoteId) {
-            this.setState({
-                title: this.props.note.title,
-                body: this.props.note.body
-            });
-        }
-    }
-
-    render() {
+  }
+  render() {
     if (this.props.note) {
       return (
         <div>
@@ -60,25 +54,24 @@ export class Editor extends React.Component {
           { this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}
         </p>
       );
-
-       }
     }
+  }
 };
 
 Editor.propTypes = {
-    note: React.PropTypes.object,
-    selectedNoteId: React.PropTypes.string,
-    call: React.PropTypes.func.isRequired,
-    browserHistory: React.PropTypes.object.isRequired
+  note: React.PropTypes.object,
+  selectedNoteId: React.PropTypes.string,
+  call: React.PropTypes.func.isRequired,
+  browserHistory: React.PropTypes.object.isRequired
 };
 
-export default createContainer (() => {
-    const selectedNoteId = Session.get('selectedNoteId');
+export default createContainer(() => {
+  const selectedNoteId = Session.get('selectedNoteId');
 
-    return {
-        selectedNoteId,
-        note: Notes.findOne(selectedNoteId),
-        call: Meteor.call
-    };
-
+  return {
+    selectedNoteId,
+    note: Notes.findOne(selectedNoteId),
+    call: Meteor.call,
+    browserHistory
+  };
 }, Editor);
