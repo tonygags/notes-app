3eac901b1111c20156816244da6446991ad5162c
Tony Gagliardi <tonygags@internode.on.net>
2017-09-03 11:48:42 +0930

    Setup mobile navigation

--- client/main.js
+++ client/main.js
@@ -16,6 +16,7 @@ Tracker.autorun(() => {
 
 Tracker.autorun(() => {
   const selectedNoteId = Session.get('selectedNoteId');
+  Session.set('isNavOpen', false);
 
   if (selectedNoteId) {
     browserHistory.replace(`/dashboard/${selectedNoteId}`);
@@ -23,7 +24,14 @@ Tracker.autorun(() => {
 
 });
 
+Tracker.autorun(() => {
+  const isNavOpen = Session.get('isNavOpen');
+  
+  document.body.classList.toggle('is-nav-open', isNavOpen);
+});
+
 Meteor.startup(() => {
   Session.set('selectNoteId', undefined);
+  Session.set('isNavOpen', false);
   ReactDOM.render(routes, document.getElementById('app'));
 });
